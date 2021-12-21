56e328ba8748f8a8d946e55ecec046b6a8d49b38
shankar <shankars0899@gmail.com>
2021-12-21 09:37:52 +0530

    handling output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+//importing the linear regression file
 mod linear_regression;
 
 fn main() {
-    println!("Iris petal classification! with accuracy {}", linear_regression::regression());
+    //getting the output from the linear regression function
+    let regression_eval: [f32; 2] = linear_regression::linear_regression();
+    //printing out the values
+    println!(
+        "Breast cancer with train {} and test accuracy {}",
+        regression_eval[0], regression_eval[1]
+    );
 }
