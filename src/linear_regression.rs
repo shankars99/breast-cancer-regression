03fe642da28f3460ffa53700c453dfa44c880bf9
shankar <shankars0899@gmail.com>
2021-12-21 09:38:02 +0530

    using Kfold

--- src/linear_regression.rs
+++ src/linear_regression.rs
@@ -1,21 +1,40 @@
-use smartcore::dataset::iris::load_dataset;
+//importing breast cancer dataset
+use smartcore::dataset::breast_cancer;
+
+//importing dense matrix and logistic regression
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
 use smartcore::linear::logistic_regression::LogisticRegression;
+
+//importing accuracy metric and Kfold cross validation
 use smartcore::metrics::accuracy;
+use smartcore::model_selection::{cross_validate, KFold};
 
-pub fn regression() -> f32 {
+//function returns an array of 2 elements: train accuracy and test accuracy
+pub fn linear_regression() -> [f32; 2] {
+    //importing breast cancer dataset
+    let breast_cancer_data = breast_cancer::load_dataset();
 
-    let iris_data = load_dataset();
-
+    //creating a dense matrix from the imported dataset
     let x = DenseMatrix::from_array(
-        iris_data.num_samples,
-        iris_data.num_features,
-        &iris_data.data,
+        breast_cancer_data.num_samples,
+        breast_cancer_data.num_features,
+        &breast_cancer_data.data,
     );
-    let y = iris_data.target;
 
-    let lr = LogisticRegression::fit(&x, &y, Default::default()).unwrap();
-    let y_hat = lr.predict(&x).unwrap();
+    //setting target values for the inputs
+    let y = breast_cancer_data.target;
 
-    accuracy(&y, &y_hat)
+    //creating a KFold cross validation object
+    let results = cross_validate(
+        LogisticRegression::fit,
+        &x,
+        &y,
+        Default::default(),
+        KFold::default().with_n_splits(2),
+        accuracy,
+    )
+    .unwrap();
+
+    //returning train and test accuracy
+    [results.mean_test_score(), results.mean_train_score()]
 }
